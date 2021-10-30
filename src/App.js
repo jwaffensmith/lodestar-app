9d5bfb298e112ecba69dde2338387d29e442b54b
Username <jwaffensmith@gmail.com>
2021-10-30 14:15:52 -0700

    added profile route, working

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import {
   Switch
 } from 'react-router-dom'
 import Astronauts from './components/astronauts/Astronauts';
+import ISS from './components/ISS/ISS';
+import Profile from './components/profile/Profile';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/astronauts" component={ Astronauts } />
+        <Route path="/ISS" component={ ISS } />
+        <Route path="/profile" component={ Profile } />
       </Switch>
     </div>
   );
---
+++ src/components/ISS/ISS.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ISS extends Component {
+    render () {
+        return (
+            <div>
+            ISS Route!
+            </div>
+        )
+    }
+}
+
+export default ISS;
